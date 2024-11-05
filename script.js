8b8e8958ffabed8ad8803538a650f31b6d8379af
eugenemoro <29465704+eugenemoro@users.noreply.github.com>
2024-11-05 13:36:00 +0500

    feat: add GUI, refactor logic

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+const gameLog = document.getElementById('game-log');
+const restartBtn = document.createElement('button');
+restartBtn.textContent = 'Play again!';
+restartBtn.addEventListener('click', restartGame);
+restartBtn.style = 'display: none';
+
+//Score
+const humanScoreOutput = document.getElementById('human-score');
+const computerScoreOutput = document.getElementById('computer-score');
+humanScoreOutput.textContent = humanScore;
+computerScoreOutput.textContent = computerScore;
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -9,57 +24,76 @@ function getComputerChoice() {
   }
 }  
 
-//no validation of human choice as this will further be updated when GUI added
-function getHumanChoice() {
-  const humanChoice = prompt("What's your choice? (rock, paper or scissors)");
-  return humanChoice;
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice) {
+  roundNumber++;
   humanChoice = humanChoice.toLowerCase();
   const winMsg = `You win! ${humanChoice} beats ${computerChoice}.`;
   const loseMsg = `You lose! ${computerChoice} beats ${humanChoice}.`;
   if (humanChoice === "rock" &&  computerChoice === "rock") {
-    console.log("Deuce!");
+    gameLog.textContent = `Deuce!`;
   } else if (humanChoice === "rock" &&  computerChoice === "paper") {
-    console.log(loseMsg);
+    gameLog.textContent = `${loseMsg}`;
     computerScore++;
   } else if (humanChoice === "rock" &&  computerChoice === "scissors") {
-    console.log(winMsg);
+    gameLog.textContent = `${winMsg}`;
     humanScore++;
   } else if  (humanChoice === "paper" &&  computerChoice === "rock") {
-    console.log(winMsg);
+    gameLog.textContent = `${winMsg}`;
     humanScore++;
   } else if  (humanChoice === "paper" &&  computerChoice === "paper") {
-    console.log("Deuce!");
+    gameLog.textContent = `Deuce!`;
   }  else if  (humanChoice === "paper" &&  computerChoice === "scissors") {
-    console.log(loseMsg);
+    gameLog.textContent = `${loseMsg}`;
     computerScore++;
   }else if  (humanChoice === "scissors" &&  computerChoice === "rock") {
-    console.log(loseMsg);
+    gameLog.textContent = `${loseMsg}`;
     computerScore++;
   } else if  (humanChoice === "scissors" &&  computerChoice === "paper") {
-    console.log(winMsg);
+    gameLog.textContent = `${winMsg}`;
     humanScore++;
   }  else if  (humanChoice === "scissors" &&  computerChoice === "scissors") {
-    console.log("Deuce!");
+    gameLog.textContent = `Deuce!`;
+  }
+  humanScoreOutput.textContent = humanScore;
+  computerScoreOutput.textContent = computerScore;
+  if (roundNumber >= 5) {
+    endGame();
   }
 }
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+  var humanChoice = document.getElementById('human-choice');
+  humanChoice.addEventListener('click', getHumanChoice);
+  function getHumanChoice(e) {
+      if (e.target.classList.contains('game-btn')) {
+        humanSelection = e.target.value;
+        const computerSelection = getComputerChoice();
+      playRound(humanSelection, computerSelection);
+    }
   }
+  
+}
+
+function endGame() {
   if (humanScore > computerScore) {
-    console.log(`You are the Winner! 🥳 \n Your score: ${humanScore} \n Computer score: ${computerScore}`);
-  } else {
-    console.log(`You lost the Game! 🥲 \n Your score: ${humanScore} \n Computer score: ${computerScore}`);
+    gameLog.textContent = `You are the Winner! 🥳`;
+  } else  if (humanScore < computerScore){
+    gameLog.textContent = `You lost the Game! 🥲`;
+  } else if (humanScore === computerScore && (humanScore !== 0 || computerScore || 0) ) {
+    gameLog.textContent = `No winner! 🤷🏼‍♂️`;
   }
+  gameLog.appendChild(restartBtn); 
+  restartBtn.style = 'display: block';
+}
+
+function restartGame() {
+  restartBtn.style = 'display: none';
+  gameLog.textContent = '';
+  humanScore = 0;
+  computerScore = 0;
+  roundNumber = 0;
+  humanScoreOutput.textContent = humanScore;
+  computerScoreOutput.textContent = computerScore;
 }
 
 playGame();
